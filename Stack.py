60f73ec4c9f0c782eb5b3f92cf414d800b0fdd3b
traviscui <70673209+traviscui@users.noreply.github.com>
2021-01-15 00:39:37 -0500

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -1,5 +1,9 @@
 class Stack:
-  def __init__(self):
-    self.data = [0]*32
-    self.head = 0  
-  
+    def __init__(self):
+        self.data = [0]*4
+        self.count = 0
+
+    def push(self, element):
+        self.data[self.count] = element
+        self.count += 1
+    
