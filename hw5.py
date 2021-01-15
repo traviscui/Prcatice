62dd8403cbfb5828bdf6d9516e6c388822cbd027
traviscui <70673209+traviscui@users.noreply.github.com>
2021-01-15 00:20:46 -0500

    1
    1

--- hw5.py
+++ hw5.py
@@ -1,25 +1,3 @@
-# <printing patterns>
-#
-# @author <Jiarong Cui> (jiarong8)
-# @date <10/5/2015>
-#
-# Virginia Tech Honor Code Pledge
-# On my honor:
-#
-# - I have not discussed the Python language code in my program with
-# anyone other than my instructor or the teaching assistants
-# assigned to this course.
-# - I have not used Python language code obtained from another student,
-# or any other unauthorized source, either modified or unmodified.
-# - If any Python language code or documentation used in my program
-# was obtained from another source, such as a text book of coarse
-# notes, that has been clearly noted with a proper citation in
-# the comments of my program.
-# - I have not designed this program in such a way as to defeat or
-# interfere with the normal operation of the Web-Cat Server.
-#
-# <Jiarong Cui>
-
 num = int(input("Enter an odd number width:"))
 
 print("Hill:")
