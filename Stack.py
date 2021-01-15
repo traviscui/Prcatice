3eea34d2c5c36f8885c23e8a966440235fc365da
traviscui <70673209+traviscui@users.noreply.github.com>
2021-01-15 16:37:26 -0500

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -7,3 +7,17 @@ class Stack:
         self.data[self.count] = element
         self.count += 1
     
+    def pop(self):
+        tempo = self.data[self.count]
+        self.data[self.count] = 0
+        self.count -= 1
+        return tempo
+
+    def isEmpty(self):
+        if self.count == 0:
+            return True
+        if self.count != 0:
+            return False
+    
+    def peek(self):
+        return self.data
