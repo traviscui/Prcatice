f5d37b97b6d3004638a68722efc95f56a16ee6d1
traviscui <70673209+traviscui@users.noreply.github.com>
2021-01-15 21:03:00 -0500

    Update MortgageCalculator.java

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -4,23 +4,26 @@ import java.util.Scanner;
 public class MortgageCalculator {
 
 	public static void main(String[] args) {
-		Scanner scanner1 = new Scanner(System.in);
+		final byte MONTH_IN_YEAR = 12;
+		final byte PERCENTAGE = 100;
+		Scanner scanner = new Scanner(System.in);
 		System.out.print("Total loan: ");
-		int loan = scanner1.nextInt();
+		int loan = scanner.nextInt();
 		
-		Scanner scanner2 = new Scanner(System.in);
 		System.out.print("Annual Interest Rate: ");
-		float aPR = scanner2.nextFloat();
+		float aPR = scanner.nextFloat();
 		
-		Scanner scanner3 = new Scanner(System.in);
 		System.out.print("Terms of loan(Years): ");
-		byte term = scanner3.nextByte();
+		byte term = scanner.nextByte();
 		
-		float monthAPR = aPR / 1200;
-		int termM = term * 12;
+		float monthAPR = aPR / (MONTH_IN_YEAR * PERCENTAGE);
+		int termM = term * MONTH_IN_YEAR;
+		
 		double mortGage = loan * (monthAPR * Math.pow((1+monthAPR), termM) / (Math.pow((1+monthAPR), termM)-1));
+		
 		NumberFormat currency = NumberFormat.getCurrencyInstance();
 		String result = currency.format(mortGage);
+		
 		System.out.println("Mortgage is " + result);
 	}
 
