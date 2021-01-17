cad3107bfc8b67d8cfa933db81a136c76e0687ef
traviscui <70673209+traviscui@users.noreply.github.com>
2021-01-17 16:45:06 -0500

    Update MortgageCalculator.java

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -2,57 +2,66 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class MortgageCalculator {
-
+	
+	final static byte MONTH_IN_YEAR = 12;
+	final static byte PERCENTAGE = 100;
+	
 	public static void main(String[] args) {
-		final byte MONTH_IN_YEAR = 12;
-		final byte PERCENTAGE = 100;
+		int loan = (int) GetNumber("Number of loan: ", 999, 1000000);
+		float aPR = (float) GetNumber("Number of Annual Interest: ", 0, 30);
+		byte term = (byte) GetNumber("Number of term(Years): ", 0, 30);
+		
+		NumberFormat currency = NumberFormat.getCurrencyInstance();
+		double mortgage = FindMortgage(loan, aPR, term);
+		String result = currency.format(mortgage);
+		
+		System.out.println("-------------------------------");
+		System.out.println("Monthly Payments is " + result);
+		System.out.println("Payment Schedule");
+		System.out.println("-------------------------------");
+		FindMonthlyPayment(loan, aPR, term);
+	}
+	
+	public static double GetNumber(String prompt, int min, int max) {
 		Scanner scanner = new Scanner(System.in);
-		int loan = 0;
-		float aPR = 0;
-		byte term = 0;
-		
 		while (true) {
-			System.out.print("Total loan: ");
-			loan = scanner.nextInt();
-			if (loan < 1000 || loan > 1000000) {
-				System.out.println("Enter a number between 1,000 and 1,000,000.");
-				continue;
-			}
-			else 
-				break;
-		} 
-		
-		while (true) {
-			System.out.print("Annual Interest Rate: ");
-			aPR = scanner.nextFloat();
-			if (aPR <= 0 || aPR > 30) {
-				System.out.println("Enter a number greater than 0 and less or equal to 30.");
-				continue;
-			}
-			else 
-				break;
-		}
-		
-		while (true) {
-			System.out.print("Terms of loan(Years): ");
-			term = scanner.nextByte();
-			if (term < 1 || term > 30) {
-				System.out.println("Enter a value between 1 and 30.");
+			System.out.print(prompt);
+			double value = scanner.nextDouble();
+			if (value <= min || value > max) {
+				System.out.println("Enter a value between " + min + " and " + max);
 				continue;
 			}
 			else
-				break;
+				return value;
 		}
 		
+	}
+	
+	public static double FindMortgage(int loan, float aPR, byte term) {		
 		float monthAPR = aPR / (MONTH_IN_YEAR * PERCENTAGE);
 		int termM = term * MONTH_IN_YEAR;
 		
 		double mortGage = loan * (monthAPR * Math.pow((1+monthAPR), termM) / (Math.pow((1+monthAPR), termM)-1));
 		
-		NumberFormat currency = NumberFormat.getCurrencyInstance();
-		String result = currency.format(mortGage);
-		
-		System.out.println("Mortgage is " + result);
+		return mortGage;
 	}
-
+	public static void FindMonthlyPayment(int loan, float aPR, byte term) {	
+		int termM = term * MONTH_IN_YEAR;
+		float monthAPR = aPR / (MONTH_IN_YEAR * PERCENTAGE);
+		
+		short timesPaid = 0;
+		
+		while (true) {
+			double balance = loan * (((Math.pow(1+monthAPR, termM)-Math.pow(1+monthAPR, timesPaid))) / (Math.pow(1+monthAPR, termM)-1));
+			timesPaid += 1;
+			if (balance < 0) 
+				break;
+			else{
+				NumberFormat currency = NumberFormat.getCurrencyInstance();
+				String result = currency.format(balance);
+				System.out.println(result);
+				continue;
+			}
+	}
+		}
 }
