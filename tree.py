72f8e54d169876f618fd6a675dd9b5c8671bda3a
traviscui <70673209+traviscui@users.noreply.github.com>
2021-01-18 23:40:48 -0500

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,6 +1,44 @@
 class Node:
     def __init__(self, value):
-        self.ancester = None
+        self.ancestor = None
         self.value = value
         self.left_child = None
         self.right_child = None
+
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def is_root(self, node):
+        if node.ancester is None:
+            return True
+        else:
+            return False
+
+    def root(self):
+        return self.root
+
+    def parent(self, node):
+        if node.ancester is None:
+            raise Exception(node + " is root.")
+        else:
+            return node.ancester
+
+    def children(self, node):
+        if BST.is_external(node) is True:
+            print("Node is external.")
+        else:
+            print("Children are " + node.left_child + node.right_child)
+
+    def is_internal(self, node):
+        if node.left_child is not None:
+            return True
+        else:
+            return False
+
+    def is_external(self, node):
+        if BST.is_internal(node) is False:
+            return True
+        else:
+            False
